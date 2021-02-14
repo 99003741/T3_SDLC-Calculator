5b411c0cbf514dd615128d7706163352329d0650
99003739 <78853339+99003739@users.noreply.github.com>
2021-02-14 18:21:53 +0530

    addition test cases added

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -38,6 +38,12 @@ int main()
 void test_add(void) {
   TEST_ASSERT_EQUAL(30, add(10, 20));
   TEST_ASSERT_EQUAL(-10, add(10, -20));
+  TEST_ASSERT_EQUAL(-20, add(10, -30));
+  TEST_ASSERT_EQUAL(-30, add(0, -30));
+  TEST_ASSERT_EQUAL(10, add(10, 0));
+  TEST_ASSERT_EQUAL(-20, add(10, -30));
+  TEST_ASSERT_EQUAL(2, add(1, 1));
+  TEST_ASSERT_EQUAL(0, add(1, 1));
 }
 void test_add_testcase2(void) {
  
@@ -46,6 +52,14 @@ void test_add_testcase2(void) {
 }
 void test_subtract(void) {
   TEST_ASSERT_EQUAL(-3, subtract(0, 3));
+  TEST_ASSERT_EQUAL(3, subtract(3, 0));
+  TEST_ASSERT_EQUAL(-9, subtract(-6, 3));
+  TEST_ASSERT_EQUAL(3, subtract(6, 3));
+  TEST_ASSERT_EQUAL(0, subtract(6, 6));
+  TEST_ASSERT_EQUAL(0, subtract(0, 0));
+  TEST_ASSERT_EQUAL(3, subtract(16, 13));
+  TEST_ASSERT_EQUAL(-3, subtract(13, 16));
+  TEST_ASSERT_EQUAL(3, subtract(6, 3));
   
   /* Dummy fail*/
   TEST_ASSERT_EQUAL(1, subtract(1000, 900));
