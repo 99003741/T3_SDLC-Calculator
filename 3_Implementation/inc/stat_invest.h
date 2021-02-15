d388d551b15374ec22f577dfe1eeda03b84105c7
99003741 <78871103+99003741@users.noreply.github.com>
2021-02-15 09:17:40 +0530

    Added Statistics and Investment operations code

---
+++ 3_Implementation/inc/stat_invest.h
@@ -0,0 +1,19 @@
+#ifndef STAT_INVEST_H_INCLUDED
+#define STAT_INVEST_H_INCLUDED
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+double p, t, r, si, ci;
+void invest();
+double simple_interest();
+double compound_interest();
+int length;
+void stat();
+double meann();
+double mediann();
+double modee();
+
+
+
+#endif // STAT_INVEST_H_INCLUDED
---
+++ 3_Implementation/src/stat_invest.c
@@ -0,0 +1,126 @@
+#include "stat_invest.h"
+void invest()
+{
+
+printf("Enter principal amount (p): ");
+scanf("%lf", &p);
+printf("Enter time in year (t): ");
+scanf("%lf", &t);
+printf("Enter rate in percent (r): ");
+scanf("%lf", &r);
+
+
+    int choice;
+    printf("enter choice\n1.Simple Interest\n2.Compound Interest\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+    case 1:
+            simple_interest(p, t, r);
+            break;
+    case 2:
+            compound_interest(p, t, r);
+            break;
+    default: printf("Error\n");
+    }
+return(0);
+}
+
+double simple_interest(double pr, double ti, double ra)
+{
+    si = (pr * ti * ra)/100.0;     /* Calculating simple interest */
+    printf("Simple Interest = %0.3f\n", si);
+}
+
+double compound_interest(double pr, double ti, double ra)
+{
+    ci = pr * (pow(1+ra/100, ti) - 1); /* Calculating compound interest */
+    printf("Compound Interest = %0.3f", ci);
+}
+
+
+//investment program
+void stat()
+{
+    int i;
+    double input[100];
+    int choice;
+
+    printf("enter choice\n1.Mean\n2.Median\n3.Mode\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+    case 1:
+            printf("Enter Array Length\n");
+            scanf("%d",&length);
+            printf("\nEnter array\n");
+            for(i=0;i<length;i++)
+            {
+
+                scanf("%lf",&input[i]);
+            }
+            meann(input);
+            break;
+    case 2:
+            printf("Enter Array Length\n");
+            scanf("%d",&length);
+            printf("\nEnter array\n");
+            for(i=0;i<length;i++)
+            {
+
+                scanf("%lf",&input[i]);
+            }
+            //mediann(input);
+            break;
+    case 3:
+            printf("Enter Array Length\n");
+            scanf("%d",&length);
+            printf("\nEnter array\n");
+            for(i=0;i<length;i++)
+            {
+
+                scanf("%lf",&input[i]);
+            }
+            modee(input);
+            break;
+
+
+    }
+}
+
+double meann(double array[100])
+{
+ double mean,sum=0;
+ int i;
+ for(i=0;i<length;i++)
+ {
+     sum+=array[i];
+ }
+ mean=sum/length;
+ printf("mean is %lf\n",mean);
+ return mean;
+}
+double modee(double array[1000])
+{
+    double mode;
+    int i,j,count,count1=1;
+    for(i=0;i<length;i++)
+    {
+        for(j=0;j<length;j++)
+        {
+            if(array[i]==array[j])
+            {
+                count++;
+            }
+
+        }
+        if(count>count1)
+        {
+
+            count1=count;
+            mode=array[i];
+        }
+    }
+    printf("Mode : %lf", mode);
+
+}
