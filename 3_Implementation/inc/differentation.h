d7e319143d7906467f127f99a14b76c036df4114
99003740 <78853299+99003740@users.noreply.github.com>
2021-03-10 23:00:05 +0530

    differentation

---
+++ 3_Implementation/inc/differentation.h
@@ -0,0 +1,19 @@
+#ifndef DIFFERENTATION_H_INCLUDED
+#define DIFFERENTATION_H_INCLUDED
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+
+int diff();
+float x, a[10], y1, dy1;
+int deg, i;
+float p;
+int i;
+float poly(float a[], int, float);
+
+float deriv(float a[], int, float);
+
+
+#endif
